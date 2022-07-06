34d2e7ea1e65d5c6b8c17464d5af22889e6674ca
badjokeresult <badjokeresult@gmail.com>
2022-07-06 21:17:26 +0300

    finished

--- task3.py
+++ task3.py
@@ -88,4 +88,3 @@ if __name__ == '__main__':
     for i, test in enumerate(tests):
         test_answer = appearance(test['data'])
         assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
-    print('Successful!')
