3227fe4d25fa8ed1588404b893dfb8e9cc1ad4ac
badjokeresult <badjokeresult@gmail.com>
2022-07-06 21:14:42 +0300

    finished

--- task2.py
+++ task2.py
@@ -1,28 +1,69 @@
-def task(x1, y1, x2, y2, x3, y3, x4, y4):
-    xA = [x1, x2]
-    xB = [x3, x4]
-    yA = [y1, y2]
-    yB = [y3, y4]
+# В нашей школе мы не можем разглашать персональные данные пользователей,
+# Но чтобы преподаватель и ученик смогли объяснить нашей поддержке,
+# Кого они имеют в виду (у преподавателей, например, часто учится несколько Саш),
+# Мы генерируем пользователям уникальные и легко произносимые имена.
+#
+# Имя у нас состоит из прилагательного, имени животного и двузначной цифры.
+# В итоге получается, например, "Перламутровый лосось 77".
+#
+# Для генерации таких имен мы и решали следующую задачу:
+# Получить с русской википедии список всех животных (https://inlnk.ru/jElywR)
+# И вывести количество животных на каждую букву алфавита. Результат должен получиться в следующем виде:
+#
+# А: 642
+# Б: 412
+# В:....
 
-    if max(xA) >= min(xB) and min(xA) <= min(xB):
-        dx = max(xA) - min(xB)
-        dy = 0
-        if max(yA) >= min(yB) and min(yA) <= min(yB):
-            dy = max(yA) - min(yB)
-        return dx * dy
-    elif max(xB) >= min(xA) and min(xB) <= min(xA):
-        dx = max(xB) - min(xA)
-        dy = 0
-        if max(yB) >= min(yA) and min(yB) <= min(yA):
-            dy = max(yB) - min(yA)
-        return dx * dy
-    return False
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class WikiParser:
+    url_prefix = r'https://ru.wikipedia.org'
+    dictionary = {letter: 0 for letter in 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'}
+
+    def __init__(self):
+        self.page = requests.get(r'https://inlnk.ru/jElywR')
+        self.bs = BeautifulSoup(self.page.text, 'html.parser')
+
+    def get_new_url(self):
+        block = self.bs.find('div', id='mw-pages')
+        urls = block.find_all('a', href=True)
+        for url in urls:
+            if url.text == 'Следующая страница':
+                self.page = requests.get(self.url_prefix + url['href'])
+                self.bs = BeautifulSoup(self.page.text, 'html.parser')
+                return True
+        return False
+
+    def parse(self):
+        list_part = self.bs.find('div', class_='mw-category mw-category-columns')
+        blocks = list_part.find_all('div', class_='mw-category-group')
+        for block in blocks:
+            letter = block.find('h3').text
+            animals = block.find_all_next('li')
+            if letter in self.dictionary:
+                self.dictionary[letter] += len(animals)
+            else:
+                return False
+        return True
+
+    def __str__(self):
+        result = ''
+        for key, value in self.dictionary.items():
+            result += f'{key}: {value}\n'
+        return result
+
+
+def task():
+    parser = WikiParser()
+    while True:
+        if parser.parse() and parser.get_new_url():
+            continue
+        break
+    print(parser)
 
 
 if __name__ == '__main__':
-    print(task(1, 1, 2, 2, 3, 3, 4, 4))
-    print(task(-5, 2, 3, -2, 2, 6, 5, 1))
-    print(task(1, 4, 6, 1, 5, 5, 7, 3))
-    print(task(5, 5, 7, 3, 1, 4, 6, 1))
-    print(task(-2, 3, 0, -2, 0, -2, 2, -4))
-    print(task(2, 5, 6, 1, 3, 4, 5, 2))
+    task()
--- task3.py
+++ task3.py
@@ -1,50 +1,91 @@
-import requests
-from bs4 import BeautifulSoup
+# Мы сохраняем время присутствия каждого пользователя на уроке в виде интервалов.
+
+# В функцию передается словарь, содержащий три списка с таймстемпами (время в секундах):
+#  - lesson – начало и конец урока
+#  - pupil – интервалы присутствия ученика
+#  - tutor – интервалы присутствия учителя
+
+# Интервалы устроены следующим образом – это всегда список из четного количества элементов.
+# Под четными индексами (начиная с 0) время входа на урок, под нечетными - время выхода с урока.
+
+# Нужно написать функцию,
+# Которая получает на вход словарь с интервалами
+# И возвращает время общего присутствия ученика и учителя на уроке (в секундах).
 
 
-def get_new_url(bs: BeautifulSoup):
-    main_part = bs.find('div', id='mw-pages')
-    possible_urls = main_part.find_all('a', href=True)
-    for url in possible_urls:
-        if url.text == 'Следующая страница':
-            return url['href']
-    raise Exception()
+
+def appearance(intervals):
+    lesson = merge_intervals(intervals['lesson'])
+    pupil = merge_intervals(intervals['pupil'])
+    tutor = merge_intervals(intervals['tutor'])
+
+    compress_intervals(pupil, lesson)
+    compress_intervals(tutor, lesson)
+
+    start_time = max(lesson[0], pupil[0], tutor[0])
+    end_time = min(lesson[-1], pupil[-1], tutor[-1])
+
+    result = end_time - start_time
+
+    result -= absence_time(pupil, lesson)
+    result -= absence_time(tutor, lesson)
+
+    return result
 
 
-def parse(dct: dict, url: str):
-    page = requests.get(url)
-    bs = BeautifulSoup(page.text, 'html.parser')
+def merge_intervals(intervals):
+    res = []
+    tupled = [(intervals[i], intervals[i + 1]) for i in range(0, len(intervals), 2)]
+    tupled.sort(key=lambda x: x[0])
+    result = [tupled[0]]
+    for i in range(1, len(tupled)):
+        cur = tupled[i]
+        if cur[0] <= result[-1][1]:
+            old = result[-1]
+            result.pop()
+            result.append([old[0], max(cur[1], old[1])])
+        else:
+            result.append(cur)
+    for item in result:
+        res.extend(list(item))
+    return res
 
-    parts = bs.find_all('div', class_='mw-category-group')
-    for part in parts:
-        possible_letters = part.find_all_next('h3')
-        letter = None
-        for let in possible_letters:
-            if len(let.text) == 1:
-                letter = let.text
-                break
-        dct[letter] += len(part.find_all_next('li'))
 
-    return bs
+def compress_intervals(intervals, lesson):
+    for i in range(len(intervals)):
+        if intervals[i] < lesson[0]:
+            intervals[i] = lesson[0]
+        elif intervals[i] > lesson[1]:
+            intervals[i] = lesson[1]
 
 
-def task():
-    alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
-    dct = {let: 0 for let in alphabet}
-    url_prefix = r'https://ru.wikipedia.org'
+def absence_time(intervals, lesson):
+    time = 0
+    for i in range(2, len(intervals), 2):
+        time += min(intervals[i], lesson[1]) - min(intervals[i - 1], lesson[1])
+    return time
 
-    bs = parse(dct, r'https://inlnk.ru/jElywR')
-    while True:
-        try:
-            url = get_new_url(bs)
-            bs = parse(dct, url_prefix + url)
-        except:
-            break
 
-    return dct
-
+tests = [
+    {'data': {'lesson': [1594663200, 1594666800],
+             'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
+             'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
+     'answer': 3117
+    },
+    {'data': {'lesson': [1594702800, 1594706400],
+             'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512, 1594704513, 1594704564, 1594705150, 1594704581, 1594704582, 1594704734, 1594705009, 1594705095, 1594705096, 1594705106, 1594706480, 1594705158, 1594705773, 1594705849, 1594706480, 1594706500, 1594706875, 1594706502, 1594706503, 1594706524, 1594706524, 1594706579, 1594706641],
+             'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705149, 1594706463]},
+    'answer': 3577
+    },
+    {'data': {'lesson': [1594692000, 1594695600],
+             'pupil': [1594692033, 1594696347],
+             'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
+    'answer': 3565
+    },
+]
 
 if __name__ == '__main__':
-    result = task()
-    for key in result:
-        print(f'{key}: {result[key]}')
+    for i, test in enumerate(tests):
+        test_answer = appearance(test['data'])
+        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    print('Successful!')
